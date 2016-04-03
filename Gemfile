6dfbf098afe23a41e8bff9c381f873618767045b
monsteronfire <monsteronfire@gmail.com>
2016-04-03 23:50:27 +0800

    Replace foundation with bootstrap

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Foodback Germs
+gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'devise'
 gem 'better_errors'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require foundation
 //= require turbolinks
 //= require_tree .
-
-$(function(){
-  $(document).foundation();
-});
