8f087ff42aec56a6c36f1b0af478c87dccd12ca1
monsteronfire <monsteronfire@gmail.com>
2016-04-07 07:59:21 +0800

    Add methods in restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def updates
+  end
+
+  def destroy
+  end
 end
