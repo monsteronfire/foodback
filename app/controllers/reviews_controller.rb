48e1e0bea724fc7c83c6a8c0535396e0007429a5
monsteronfire <monsteronfire@gmail.com>
2016-04-17 16:38:05 +0800

    add reviews new page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(restaurant: @restaurant)
+  end
 end
